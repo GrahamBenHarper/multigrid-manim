19e5e711a7b3c3482c9d5b85ec0d4164d47ece81
Graham Harper <grahambenharper@gmail.com>
2023-10-14 13:23:14 -0600

    Add HeatEquationMultigrid scene, add more smoothers, and polish JacobiFrequency scene

--- multigrid.py
+++ multigrid.py
@@ -27,15 +27,38 @@ def createSineVector(coordinates: np.array) -> np.array:
 def createRandomVector(coordinates: np.array) -> np.array:
   return np.random.rand(len(coordinates))
 
-# apply the smoother x = x + omega*Dinv(b-Ax)
-def applySmoother(x: np.array, b: np.array, A: np.array, omega: float) -> np.array:
+# apply the Jacobi smoother x = x + omega*Dinv(b-Ax)
+def applyJacobi(x: np.array, b: np.array, A: np.array, omega: float) -> np.array:
+  """
+  Apply a Jacobi smoother to the input array x based on the matrix A, rhs b, weight omega.
+  @param x The input vector
+  @param b The right-hand side of the equation being solved, Ax=b
+  @param A The matrix for the equation being solved, Ax=b
+  @param omega The weighting parameter for the update
+  """
   Dinv = np.diag(1./np.diag(A))
   r = b - np.matmul(A,x)
   return x + omega*np.matmul(Dinv,r)
 
+# apply the Gauss-Seidel smoother x = x + omega*Linv(b-Ax)
+def applyGaussSeidel(x: np.array, b: np.array, A: np.array, omega: float) -> np.array:
+  """
+  Apply a Gauss-Seidel smoother to the input array x based on the matrix A, rhs b, weight omega.
+  @param x The input vector
+  @param b The right-hand side of the equation being solved, Ax=b
+  @param A The matrix for the equation being solved, Ax=b
+  @param omega The weighting parameter for the update
+  """
+  Linv = np.linalg.inv(np.tril(A))
+  r = b - np.matmul(A,x)
+  return x + omega*np.matmul(Linv,r)
+
 # apply a linear interpolation restrictor matrix-free
 # Briggs uses a factor of 1/4 for R
 def applyLinearInterpR(x: np.array) -> np.array:
+  """
+  Apply a 1D linear interpolation R to a vector x, from Briggs
+  """
   if(len(x)%2==0):
     print("Attempting to apply LinearInterpP on a vector with {} entries! Only odd numbers of entries are supported!".format(len(x)))
   
@@ -49,11 +72,14 @@ def applyLinearInterpR(x: np.array) -> np.array:
 # apply a linear interpolation prolongator matrix-free
 # Briggs uses a factor of 1/2 for P
 def applyLinearInterpP(x: np.array) -> np.array:
+  """
+  Apply a 1D linear interpolation P to a vector x, from Briggs
+  """
   xout = np.zeros(2*len(x)+1)
 
   scale = 2. #2.*math.sqrt(2.)
   xout[0] = x[0]/scale
-  for i in range(0,len(x)):
+  for i in range(0,len(x)-1):
     xout[2*i+1] = 2*x[i]/scale
     xout[2*i+2] = (x[i]+x[i+1])/scale
   
@@ -62,9 +88,16 @@ def applyLinearInterpP(x: np.array) -> np.array:
 # show how Jacobi affects the breakdown of frequency
 class JacobiFrequency(Scene):
   def construct(self):
-    title = MathTex(r"\mbox{Solve} Ax=b").to_edge(UP)
-    r_label = MathTex(r"r = b - Ax").to_edge(LEFT)
-    self.play(Write(title),Write(r_label))
+#    title = MathTex(r"\mbox{Solve} Ax=b").to_edge(UP)
+    title = Text("Jacobi's Method - Laplace's Equation").scale(0.8).to_edge(UP)
+    jacobitext = MathTex(r"\mathbf{x} = \mathbf{x} + \omega D^{-1}(\mathbf{b}-A\mathbf{x})").scale(0.8).shift(2.5*UP)
+    smoothtext = Text("Low Frequency Mode").scale(0.5).to_edge(DOWN).shift(3*LEFT)
+    randtext = Text("High Frequency Mode").scale(0.5).to_edge(DOWN).shift(3*RIGHT)
+    r_label = MathTex(r"r = b - Ax").scale(0.8).shift(4.2*LEFT+1.2*UP)
+    self.play(Write(title),Write(r_label),Write(smoothtext),Write(randtext),Write(jacobitext))
+    
+    it_text = Text("Iteration: 0").scale(0.8)
+    self.add(it_text)
 
     # RNG seed for reproducibility
     np.random.seed(42)
@@ -77,7 +110,7 @@ class JacobiFrequency(Scene):
     # use the answer to form the RHS
     b = np.matmul(A,u)
     # create an initial guess, but pre-break the pieces by smooth and non-smooth components
-    xrand = np.random.rand(n)/10.
+    xrand = np.random.rand(n)/4.
     xsmooth = 4.*np.sin(8.*math.pi*x_coords) + 4.*np.sin(4*math.pi*x_coords) + 0.*np.cos(2*math.pi*x_coords)
     x = xrand+xsmooth
     # compute residual
@@ -89,10 +122,47 @@ class JacobiFrequency(Scene):
       y_range=[-n**2, n**2, 1000], # note: last label in the range won't show up if using np.arange(0,5,1)
       x_axis_config={"numbers_to_include": np.arange(0, 1.1, 1)},#      y_axis_config={"numbers_to_include": np.arange(-1, 1.1, 0.4)},
       tips=False,
-    ).scale(0.5).shift(0.8*RIGHT)
+    ).scale(0.5).shift(UP)
     graph_main = ax_main.plot_line_graph(x_values=x_coords, y_values=r, line_color=BLUE, add_vertex_dots=False)
 
+    rsmooth = b - np.matmul(A,xsmooth)
+    ax_smooth = ax_main.copy().shift(3.*DOWN+3.5*LEFT)
+    graph_smooth = ax_smooth.plot_line_graph(x_values=x_coords, y_values=rsmooth, line_color=BLUE, add_vertex_dots=False)
+
+    rrand = b - np.matmul(A,xrand)
+    ax_rand = ax_main.copy().shift(3.*DOWN+3.5*RIGHT)
+    graph_rand = ax_rand.plot_line_graph(x_values=x_coords, y_values=rrand, line_color=BLUE, add_vertex_dots=False)
+
     self.play(Create(ax_main),Create(graph_main))
+    self.play(Create(ax_smooth),Create(graph_smooth))
+    self.play(Create(ax_rand),Create(graph_rand))
+    self.remove(graph_smooth,graph_rand,graph_main)
+    
+    num_its = 30
+    for it in range(0,num_its):
+      xsmooth = applyJacobi(xsmooth,b,A,2./3.)
+      rsmooth = b - np.matmul(A,xsmooth)
+      graph_smooth_new = ax_smooth.plot_line_graph(x_values=x_coords, y_values=rsmooth, line_color=BLUE, add_vertex_dots=False)
+      xrand = applyJacobi(xrand,b,A,2./3.)
+      rrand = b - np.matmul(A,xrand)
+      graph_rand_new = ax_rand.plot_line_graph(x_values=x_coords, y_values=rrand, line_color=BLUE, add_vertex_dots=False)
+      x = xrand + xsmooth
+      r = b - np.matmul(A,x)
+      graph_main_new = ax_main.plot_line_graph(x_values=x_coords, y_values=r, line_color=BLUE, add_vertex_dots=False)
+
+      self.remove(it_text)
+      it_text = Text("Iteration: {}".format(it+1)).scale(0.8)
+      self.add(it_text)
+      
+      self.play(ReplacementTransform(graph_smooth,graph_smooth_new),ReplacementTransform(graph_rand,graph_rand_new),ReplacementTransform(graph_main,graph_main_new),run_time=0.2)
+      self.remove(graph_smooth_new)
+      self.remove(graph_rand_new)
+      self.remove(graph_main_new)
+
+    self.add(graph_smooth_new)
+    self.add(graph_rand_new)
+    self.add(graph_main_new)
+
     self.wait(3)
 
 # Defines a piecewise linear function from [0,1] to the convex hull of the input points
@@ -277,7 +347,7 @@ class JacobiSmoother(Scene):
     graphs = [graph]
 
     # apply smoother
-    new_y = applySmoother(graph_y,b,A,0.5)
+    new_y = applyJacobi(graph_y,b,A,0.5)
     graphs.append(ax.plot_line_graph(x_values=graph_x, y_values=new_y, line_color=BLUE, add_vertex_dots=False))
 
     # animate it!
@@ -290,7 +360,7 @@ class JacobiSmoother(Scene):
     self.play(Transform(graphs[0],graphs[1]))
     for it in range(0,100):
       graph_y = new_y
-      new_y = applySmoother(graph_y,b,A,0.6)
+      new_y = applyJacobi(graph_y,b,A,0.6)
       graphs.append(ax.plot_line_graph(x_values=graph_x, y_values=new_y, line_color=BLUE, add_vertex_dots=False))
       self.remove(graphs[it])
       self.remove(it_text)
@@ -378,7 +448,7 @@ class HeatEquation(Scene):
     # Animate Jacobi
     # first 10 iterations are done one at a time
     for it in range(1,min(10,num_its)):
-      graph_y = applySmoother(graph_y,b,A,2./3.)
+      graph_y = applyJacobi(graph_y,b,A,2./3.)
       new_graph = ax.plot_line_graph(x_values=graph_x, y_values=graph_y, line_color=BLUE, add_vertex_dots=False)
       self.remove(it_text)
       it_text = Text(f"Iteration: {it}").scale(0.5).shift(1.8*UP)
@@ -391,7 +461,7 @@ class HeatEquation(Scene):
     # now step 5 at a time
     for it in range(min(10,num_its),num_its,5):
       for subit in range(0,5):
-        graph_y = applySmoother(graph_y,b,A,2./3.)
+        graph_y = applyJacobi(graph_y,b,A,2./3.)
       new_graph = ax.plot_line_graph(x_values=graph_x, y_values=graph_y, line_color=BLUE, add_vertex_dots=False)
       self.remove(it_text)
       it_text = Text(f"Iteration: {it}").scale(0.5).shift(1.8*UP)
@@ -402,5 +472,95 @@ class HeatEquation(Scene):
       self.remove(new_graph)
     self.add(new_graph)
     conc_text = Text("Jacobi's method only transmits information locally!",color=BLUE).scale(0.6).shift(3*DOWN)
+    self.play(Create(conc_text))
+    self.wait(3)
+
+class HeatEquationMultigrid(Scene):
+  def construct(self):
+    # Generic setup
+    title_text = Text("Jacobi's Method — Clamped Heat Equation").scale(0.8).shift(3*UP)
+    it_text = Text("Iteration: 0").scale(0.5).shift(1.8*UP)
+    eq_text = MathTex(r"\mathbf{x} = \mathbf{x} + \omega D^{-1}(\mathbf{b}-A\mathbf{x})").scale(0.8).shift(2.4*UP)
+
+    # Physics setup
+    # heat equation with left and right ends clamped, no forcing
+    bc_left = 0.4
+    bc_right = 1.0
+
+    # Mesh/discretization setup
+    n = 101
+    h = 1./(n-1)
+    graph_x = np.linspace(0,1,n)
+    graph_y = graph_x*0.
+    graph_y[0] = bc_left
+    graph_y[-1] = bc_right
+    n_text = Text(f"n = {n} variables").scale(0.5).shift(2*DOWN)
+    self.play(Write(title_text),Write(it_text),Write(eq_text),Write(n_text))
+    
+    # Matrix and RHS setup
+    num_its = 5
+    A = create1dLaplaceMatrix(n)
+    b = graph_y
+    sol = np.linalg.solve(A,b)    
+    
+    # Setup axes
+    ax = Axes(
+      x_range=[0, 1.1, 0.2],
+      y_range=[-1.1, 1.1, 0.2],
+      # note: last label in the range won't show up if using np.arange(0,5,1)
+      x_axis_config={"numbers_to_include": np.arange(0, 1.1, 1)},
+      y_axis_config={"numbers_to_include": np.arange(-1, 1.1, 0.4)},
+      tips=False,
+    ).shift(1.5*DOWN+0.5*RIGHT)
+    graph = ax.plot_line_graph(x_values=graph_x, y_values=graph_y, line_color=BLUE, add_vertex_dots=False)
+    graph_sol = ax.plot_line_graph(x_values=graph_x, y_values=sol, line_color=RED, add_vertex_dots=False)["line_graph"]
+    graph_sol = DashedVMobject(graph_sol)
+    sol_text = Text("true solution",color=RED).scale(0.4).shift(2.*LEFT+UP)
+    numsol_text = Text("numerical solution",color=BLUE).scale(0.4).shift(DOWN+LEFT)
+    box = SurroundingRectangle(graph, buff=0.05, color=WHITE)
+    bcno_text = Text("0°C").scale(0.5).align_to(box,[-1., -1., 0.]).shift(0.8*LEFT)
+    bclo_text = Text("40°C").scale(0.5).align_to(box,[-1., 0., 0.]).shift(LEFT+0.4*DOWN)
+    bchi_text = Text("100°C").scale(0.5).align_to(box,[1., 1., 0.]).shift(1.2*RIGHT)
+    self.play(Create(graph),Create(numsol_text),Create(box),Create(bcno_text),Create(bclo_text),Create(bchi_text))
+    self.wait(1)
+    self.play(Create(graph_sol),Create(sol_text))
+    self.wait(1)
+    
+    # Animate multigrid
+    # first 10 iterations are done one at a time
+    for it in range(1,num_its):
+      graph_y = applyJacobi(graph_y,b,A,2./3.)
+      new_graph = ax.plot_line_graph(x_values=graph_x, y_values=graph_y, line_color=BLUE, add_vertex_dots=False)
+      self.remove(it_text)
+      it_text = Text(f"Iteration: {it}").scale(0.5).shift(1.8*UP)
+      self.add(it_text)
+      self.remove(graph)
+      self.play(ReplacementTransform(graph,new_graph),run_time=0.3)
+
+      # coarse grid
+      coarse_y = applyLinearInterpR(graph_y)
+      coarse_x = np.linspace(0,1,len(coarse_y))
+      coarse_b = 0.*coarse_y
+      coarse_b[0] = bc_left
+      coarse_b[-1] = bc_right
+      coarse_graph = ax.plot_line_graph(x_values=coarse_x, y_values=coarse_y, line_color=GREEN, add_vertex_dots=False)
+      self.remove(new_graph)
+      self.play(ReplacementTransform(new_graph,coarse_graph))
+      
+      coarse_A = create1dLaplaceMatrix(len(coarse_y))
+      coarse_Ainv = np.linalg.inv(coarse_A)
+      coarse_y = np.matmul(coarse_Ainv,coarse_b)
+      coarse_graph_sol = ax.plot_line_graph(x_values=coarse_x, y_values=coarse_y, line_color=GREEN, add_vertex_dots=False)
+      self.remove(coarse_graph)
+      self.play(ReplacementTransform(coarse_graph,coarse_graph_sol))
+
+      fine_y = applyLinearInterpP(coarse_y)
+      graph_y = fine_y
+      graph = ax.plot_line_graph(x_values=graph_x, y_values=graph_y, line_color=BLUE, add_vertex_dots=False)
+      self.remove(coarse_graph_sol)
+      self.play(ReplacementTransform(coarse_graph_sol,graph))
+
+    self.add(new_graph)
+    conc_text = Text("Multigrid transmits information globally!",color=BLUE).scale(0.6).shift(3*DOWN)
     self.play(Create(conc_text))
     self.wait(3)
